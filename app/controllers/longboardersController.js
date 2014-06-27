29ede19e716eac88664ae5694ff82d1a808cb0fd
julestakahashi <julestakahashi@gmail.com>
2014-06-26 20:59:40 -0700

    add module and controller

---
+++ app/controllers/longboardersController.js
@@ -0,0 +1,44 @@
+(function() {
+    var LongboardersController = function LongboardersController($scope) {
+        $scope.longboarders = [
+            {"username": 'Dick Buttkiss', "location": 'Portland, OR', "age": 99, "discipline": ['downhill', 'dance']},
+            {
+                "username": 'Peter Boneween',
+                "location": 'San Francisco, CA',
+                "age": 700,
+                "discipline": ['freeride', 'dance']
+            },
+            {
+                "username": 'Zeus Booger',
+                "location": 'Seattle, Washington',
+                "age": 1,
+                "discipline": ['freeride', 'downhill']
+            },
+            {"username": 'John Doe', "location": 'Dallas, Texas', "age": 30, "discipline": ['cruising']},
+            {"username": 'Lady Bum', "location": 'New York, New York', "age": 90, "discipline": ['downhill']},
+            {"username": 'Hobo Jenkins', "location": 'Miami, Florida', "age": 89, "discipline": ['downhill', 'dance']},
+            {"username": 'Dirk Zaster', "location": 'Atlanta, Georgia', "age": 76, "discipline": ['downhill']},
+            {
+                "username": 'Stink Goblin',
+                "location": 'Baton Rouge, Louisiana',
+                "age": 3,
+                "discipline": ['downhill', 'freeride']
+            },
+            {
+                "username": 'Glerbenflerk klinkerblinki',
+                "location": 'Chicago, Illinois',
+                "age": 9007,
+                "discipline": ['freestyle', 'freeride']
+            },
+            {"username": 'wenus gobble', "location": 'San Mateo, California', "age": 24, "discipline": ['dance']},
+            {"username": 'captain fartbucket', "location": 'Anchorage, Alaska', "age": 9021, "discipline": ['dance']}
+        ];
+    };
+
+    LongboardersController.$inject = ['$scope'];
+
+    angular.module('longboardersApp')
+        .controller('LongboardersController', LongboardersController);
+
+
+}());
---
+++ app/js/main.js
@@ -0,0 +1,2 @@
+
+var longboardersApp = angular.module('longboardersApp', []);
